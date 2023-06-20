70022824989597eb840c17ce76637ad248108ff6
Mohamed <ouedraogomohamed008@gmail.com>
2023-06-20 14:28:35 +0000

    Update

--- form_script.php
+++ form_script.php
@@ -1,38 +1,51 @@
 <?php
 
-// Redirect to success page
-header("Location: success.html");
-
 // Get form data
-$last_name = $_POST['last_name'];
-$first_name = $_POST['first_name'];
-$email = $_POST['email'];
-$country = $_POST['country'];
-$profession = $_POST['profession'];
-$other_profession = $_POST['other_profession'];
-$comment = $_POST['comment'];
-$feel = $_POST['feel'];
-$look = $_POST['look'];
-$efficiency = $_POST['efficiency'];
+$last_name = htmlspecialchars($_POST['last_name']);
+$first_name = htmlspecialchars($_POST['first_name']);
+$email = htmlspecialchars($_POST['email']);
+$country = htmlspecialchars($_POST['country']);
+$profession = htmlspecialchars($_POST['profession']);
+$other_profession = htmlspecialchars($_POST['other_profession']);
+$comment = htmlspecialchars($_POST['comment']);
+$feel = htmlspecialchars($_POST['feel']);
+$look = htmlspecialchars($_POST['look']);
+$efficiency = htmlspecialchars($_POST['efficiency']);
+
+// Validate email format
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("Invalid email format");
+}
+
+// Future feature to add
+// Authenticate and authorize user
+// Add the authentication and authorization logic
+// For example, we can check if the user is logged in or has the necessary privileges
 
 // Read the existing content of the file
 $existing_content = file_get_contents('users_data.xml');
 
-// Add form data as a new line to the content
+// Prepare the new form data as XML
 $new_line = "\n" . '<form_data>' . "\n" .
-            '    <last_name>' . $last_name . '</last_name>' . "\n" .
-            '    <first_name>' . $first_name . '</first_name>' . "\n" .
-            '    <email>' . $email . '</email>' . "\n" .
-            '    <country>' . $country . '</country>' . "\n" .
-            '    <profession>' . $profession . '</profession>' . "\n" .
-            '    <other_profession>' . $other_profession . '</other_profession>' . "\n" .
-            '    <comment>' . $comment . '</comment>' . "\n" .
-            '    <feel>' . $feel . '</feel>' . "\n" .
-            '    <look>' . $look . '</look>' . "\n" .
-            '    <efficiency>' . $efficiency . '</efficiency>' . "\n" .
-            '</form_data>';
+    '    <last_name>' . $last_name . '</last_name>' . "\n" .
+    '    <first_name>' . $first_name . '</first_name>' . "\n" .
+    '    <email>' . $email . '</email>' . "\n" .
+    '    <country>' . $country . '</country>' . "\n" .
+    '    <profession>' . $profession . '</profession>' . "\n" .
+    '    <other_profession>' . $other_profession . '</other_profession>' . "\n" .
+    '    <comment>' . $comment . '</comment>' . "\n" .
+    '    <feel>' . $feel . '</feel>' . "\n" .
+    '    <look>' . $look . '</look>' . "\n" .
+    '    <efficiency>' . $efficiency . '</efficiency>' . "\n" .
+    '</form_data>';
+
+// Encode the XML data
+$new_line = htmlspecialchars($new_line);
 
 // Write the new content to the file
-file_put_contents('users_data.xml', $existing_content . $new_line);
+file_put_contents('users_data.xml', $existing_content . $new_line, LOCK_EX);
 
+// Redirect to success page
+header("Location: success.html");
+exit();
 ?>
